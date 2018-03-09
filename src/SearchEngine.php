04de0f8b73fb04a53d9b9b44b23981636d853ca3
t.mueller <thomas.mueller@geld.de>
2018-03-09 08:41:50 +0100

    add library and other bot types

---
+++ src/SearchEngine.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of the ua-browser-type package.
+ *
+ * Copyright (c) 2015-2018, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+namespace UaBrowserType;
+
+final class SearchEngine implements TypeInterface
+{
+    use BrowserType;
+
+    public const TYPE = 'search-engine';
+
+    /**
+     * the name of the browser
+     *
+     * @var string|null
+     */
+    private const NAME = 'Search Engine';
+
+    /**
+     * the Browser is a Bot
+     *
+     * @var bool
+     */
+    private const BOT = true;
+
+    /**
+     * the Browser is a Syndication Reader
+     *
+     * @var bool
+     */
+    private const READER = false;
+
+    /**
+     * the Browser uses a transcoding webservice
+     *
+     * @var bool
+     */
+    private const TRANSCODER = false;
+}
