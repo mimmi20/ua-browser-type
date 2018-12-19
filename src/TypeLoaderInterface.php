b93a8c0a6e0e7742feb078f8b5c1663ceec57aef
t.mueller <thomas.mueller@geld.de>
2018-12-19 08:30:16 +0100

    add interface

---
+++ src/TypeLoaderInterface.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the ua-browser-type package.
+ *
+ * Copyright (c) 2015-2018, Thomas Mueller <mimmi20@live.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+namespace UaBrowserType;
+
+use BrowserDetector\Loader\LoaderInterface;
+
+interface TypeLoaderInterface extends LoaderInterface
+{
+    /**
+     * @param string $key
+     *
+     * @throws \BrowserDetector\Loader\NotFoundException
+     *
+     * @return \UaBrowserType\TypeInterface
+     */
+    public function load(string $key): TypeInterface;
+}
